18d23a568c4f40ea371f42067d039a931a0c9cfb
suinain <suin961205@naver.com>
2024-08-13 09:38:06 +0900

    매퍼에 주석 달기

--- semi-erp/src/main/java/com/kh/semi/mapper/VacaReqMapper.java
+++ semi-erp/src/main/java/com/kh/semi/mapper/VacaReqMapper.java
@@ -15,19 +15,19 @@ public class VacaReqMapper implements RowMapper<VacaReqDto>{
 
 	@Override
 	public VacaReqDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-		VacaReqDto vacaReqDto = new VacaReqDto();
-		vacaReqDto.setVacaNo(rs.getInt("vaca_no"));
-		vacaReqDto.setVacaTel(rs.getString("vaca_tel"));
-		vacaReqDto.setVacaSdate(rs.getDate("vaca_sdate"));
-		vacaReqDto.setVacaEdate(rs.getDate("vaca_edate"));
-		vacaReqDto.setVacaReason(rs.getString("vaca_reason"));
-		vacaReqDto.setVacaRej(rs.getString("vaca_rej"));
-		vacaReqDto.setVacaReqDate(rs.getDate("vaca_req_date"));
-		vacaReqDto.setApproDate(rs.getDate("appro_date"));
-		vacaReqDto.setApproYN(rs.getString("appro_yn"));
-		vacaReqDto.setApproNo(rs.getInt("appro_no"));
-		vacaReqDto.setApproBos(rs.getString("appro_bos"));
-		vacaReqDto.setLoginID(rs.getString("loginID"));
+		VacaReqDto vacaReqDto = new VacaReqDto(); 
+		vacaReqDto.setVacaNo(rs.getInt("vaca_no")); //휴가번호
+		vacaReqDto.setVacaTel(rs.getString("vaca_tel")); //비상연락처
+		vacaReqDto.setVacaSdate(rs.getDate("vaca_sdate")); //휴가시작일
+		vacaReqDto.setVacaEdate(rs.getDate("vaca_edate")); //휴가종료일
+		vacaReqDto.setVacaReason(rs.getString("vaca_reason"));//휴가사유
+		vacaReqDto.setVacaRej(rs.getString("vaca_rej"));//반려사유
+		vacaReqDto.setVacaReqDate(rs.getDate("vaca_req_date"));//휴가신청일
+		vacaReqDto.setApproDate(rs.getDate("appro_date"));//결재일자
+		vacaReqDto.setApproYN(rs.getString("appro_yn")); //결재여부
+		vacaReqDto.setApproNo(rs.getInt("appro_no"));//결재번호
+		vacaReqDto.setApproBos(rs.getString("appro_bos"));//결재자
+		vacaReqDto.setLoginID(rs.getString("loginID"));//사번
 		
 		return vacaReqDto;
 	}

5a0805b99c79777a1fcbf4f86d8c56de22d806a3
jh011228 <jh01122844@gmail.com>
2024-08-26 15:04:16 +0900

    충돌

---
+++ erp/src/test/java/com/kh/erp/회원조회테스트.java
@@ -0,0 +1,15 @@
+package com.kh.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class 회원조회테스트 {
+	
+	@Test 
+	public void test() {
+		
+		System.out.println("hello java");
+	}
+}
